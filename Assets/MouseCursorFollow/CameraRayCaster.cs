03c100572c707c1230dfab568c87abb296439353
DaynerKurdi <ziad_dayner@yahoo.com>
2018-01-27 01:00:19 +0900

    #1 mouse now follow the cursor

---
+++ Assets/MouseCursorFollow/CameraRayCaster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRayCaster : MonoBehaviour {
+
+
+    public Transform MouseCursor;
+
+    // Use this for initialization
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main
+            .ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 objectHit = hit.point;
+            MouseCursor.position = new Vector3(objectHit.x, objectHit.y, objectHit.z);
+            // Do something with the object that was hit by the raycast.
+        }
+    }
+
+
+}
